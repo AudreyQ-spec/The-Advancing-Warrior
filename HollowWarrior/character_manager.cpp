978e1762967e0be814b6b304f9fcbd65fc64dce5
AudreyQ-spec <qujiayi2008@qq.com>
2025-07-19 22:46:55 +0800

    Initial commit

---
+++ HollowWarrior/character_manager.cpp
@@ -0,0 +1,46 @@
+#include "enemy.h"
+#include "player.h"
+#include "character_manager.h"
+#include "bullet_time_manager.h"
+
+CharacterManager* CharacterManager::manager = nullptr;	
+
+CharacterManager* CharacterManager::instance()
+{
+	if (!manager)
+	{
+		manager = new CharacterManager();
+	}
+	return manager;
+}
+
+void CharacterManager::on_input(const ExMessage& msg)
+{
+	player->on_input(msg);
+}
+
+void CharacterManager::on_update(float delta)
+{
+	enemy->on_update(delta);
+	player->on_update(delta);
+}
+
+void CharacterManager::on_render()
+{
+	enemy->on_render();
+	if(player->is_in_bullet_time())
+		BulletTimeManager::instance()->post_process();  // qwertyuiopasdfghjkl;Zxcvbnm,
+	player->on_render();
+}
+
+CharacterManager::CharacterManager()
+{
+	enemy = new Enemy();
+	player = new Player();
+}
+
+CharacterManager::~CharacterManager()
+{
+	delete enemy;
+	delete player;
+}
